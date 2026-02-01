1f679b58e8eaf8ba1cb5a41f283d0e7f6017015c
Abdul Salam <92994347+salamjillani@users.noreply.github.com>
2026-02-01 20:21:41 +0500

    Change background color of main div

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ const App = () => {
   }, [handleScroll]);
 
   return (
-    <div className="min-h-screen bg-[#003151]">
+    <div className="min-h-screen bg-[#051527]">
       <Navigation 
         activeSection={activeSection}
         scrollToSection={scrollToSection}
